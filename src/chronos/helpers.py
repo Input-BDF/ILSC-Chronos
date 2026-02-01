d0d3b0939c50d788f27b3eb7291f36b22a2e0058
Input-BDF <input@bdclub.de>
2026-02-01 09:57:40 +0100

    Merging Changes from throni3git/ILSC-Chronos.git tt/dev

---
+++ src/chronos/helpers.py
@@ -0,0 +1,83 @@
+import datetime as dt
+import zoneinfo
+from html.parser import HTMLParser
+from logging import Logger
+
+from chronos.config import Config
+
+
+def convert_to_date_or_timezone_datetime(date_or_datetime: dt.date | dt.datetime, time_zone: zoneinfo.ZoneInfo) -> dt.date | dt.datetime:
+    """convert to given timezone if the type is `datetime` else leave it as date."""
+
+    if type(date_or_datetime) is dt.datetime:
+        result = date_or_datetime.astimezone(time_zone)
+    elif type(date_or_datetime) is dt.date:
+        result = date_or_datetime
+    else:
+        raise ValueError(f"argument type ({type(date_or_datetime)}) not supported")
+    return result
+
+
+def convert_to_date_or_utc_datetime(
+    date_or_datetime: dt.date | dt.datetime,
+) -> dt.date | dt.datetime:
+    """convert to UTC timezone if the type is `datetime` else leave it as date."""
+
+    if type(date_or_datetime) is dt.datetime:
+        result = date_or_datetime.astimezone(zoneinfo.ZoneInfo("UTC"))
+    elif type(date_or_datetime) is dt.date:
+        result = date_or_datetime
+    else:
+        raise ValueError(f"argument type ({type(date_or_datetime)}) not supported")
+    return result
+
+
+def enable_remote_debug(app_config: Config, logger: Logger):
+    try:
+        from os import path as ospath
+
+        import netifaces as ni
+
+        logger.debug("RemoteDebug: Initializing")
+        _remote_ip = app_config.get("debug", "remote_server")
+        _remote_wd = app_config.get("debug", "remote_workdir")
+        _int_part = app_config.get("debug", "remote_iface_nr")
+        # In some cases interface has two adresses.
+        # refer https://pypi.org/project/netifaces/
+        _ifaces = ni.ifaddresses(app_config.get("debug", "remote_interface"))
+        _local_ip = _ifaces[ni.AF_INET][int(_int_part)]["addr"]
+        logger.debug(f"RemoteDebug: Running on {_local_ip}")
+        _path = ospath.dirname(ospath.abspath(__file__)).replace("/", "\\")
+    except Exception as ex:
+        logger.debug(f"RemoteDebug: Init failed: {ex}")
+        logger.debug(f"RemoteDebug: Available ifaces: {_ifaces}")
+        return
+    try:
+        import pydevd
+        from pydevd_file_utils import setup_client_server_paths
+
+        MY_PATHS_FROM_ECLIPSE_TO_PYTHON = [
+            (rf"{_remote_wd}\{_local_ip}{_path}", rf"{_path}"),
+            (rf"{_remote_wd}\{_local_ip}\usr\local\bin", r"/usr/local/bin"),
+        ]
+        setup_client_server_paths(MY_PATHS_FROM_ECLIPSE_TO_PYTHON)
+        pydevd.settrace(_remote_ip, stdoutToServer=True, stderrToServer=True)
+
+    except ImportError:
+        logger.debug("RemoteDebug: Could not import pydevd")
+    except Exception as ex:
+        logger.debug(f"RemoteDebug: General Exception {ex}")
+
+
+class HTMLFilter(HTMLParser):
+    """
+    small helper class to eliminate HTML tags from a text.
+    thanks, https://stackoverflow.com/a/55825140
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.text = ""
+
+    def handle_data(self, data):
+        self.text += data
---
+++ src/chronos/logging_helpers.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+import json
+import logging
+import logging.config
+
+from chronos.config import Config
+
+
+SUCCESS_LEVEL_NUM = 21
+
+
+def _success_logging_function(
+    self: logging.Logger,
+    message: str,
+    *args,
+    **kws,
+):
+    if self.isEnabledFor(SUCCESS_LEVEL_NUM):
+        # Yes, logger takes its '*args' as 'args'.
+        self._log(
+            SUCCESS_LEVEL_NUM,
+            message,
+            args,
+            **kws,
+        )
+
+
+def init_logging(app_config: "Config") -> None:
+    logging.addLevelName(
+        SUCCESS_LEVEL_NUM,
+        "SUCCESS",
+    )
+    LoggerClass = logging.getLoggerClass()
+    setattr(LoggerClass, "success", _success_logging_function)
+
+    # load preset for Pythons logging module
+    fn_logging = Path(__file__).absolute().parent / "_logging_settings.json"
+    if not fn_logging.exists():
+        raise FileNotFoundError(fn_logging)
+
+    with open(fn_logging, "r") as f:
+        logging_settings = json.load(f)
+
+    # resolve logging file paths and ensure parent directory existence
+    filename_logging = Path(app_config.log["path"]) / app_config.log["filename"]
+    if not filename_logging.parent.exists():
+        filename_logging.parent.mkdir()
+
+    # update logging handler with filename_logging
+    handlers = logging_settings["handlers"]
+    handlers["info_file_handler"]["filename"] = str(filename_logging)
+
+    # hand in TimedRotatingFileHandler configuration
+    handlers["info_file_handler"]["when"] = str(app_config.log["rotation"])
+    handlers["info_file_handler"]["interval"] = int(app_config.log["interval"])
+    handlers["info_file_handler"]["backupCount"] = int(app_config.log["backups"])
+
+    logging.config.dictConfig(logging_settings)
