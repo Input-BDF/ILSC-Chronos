40debc7d600a4bec8d88d805ce7fdfa78e3f2140
Thomas Thron <throni3git@gmail.com>
2026-02-22 18:57:31 +0100

    create classes for CalendarHandler

---
+++ src/chronos/base_calendar_handler.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# python lib
+from hashlib import md5
+from pathlib import Path
+from urllib.request import urlretrieve
+import datetime as dt
+import logging
+import time
+import zoneinfo
+
+# external libs
+import caldav
+import icalendar
+import x_wr_timezone
+
+# own code
+from chronos.config import Config
+from chronos.chronos_event import ChronosEvent
+
+
+logger = logging.getLogger(__name__)
+
+
+class BaseCalendarHandler:
+    def __init__(self, app_config: Config):
+        self.app_config = app_config
+
+        app_timezone = zoneinfo.ZoneInfo(self.app_config.get("app", "timezone"))
+        self.last_check = (dt.datetime.now() - dt.timedelta(days=7)).astimezone(app_timezone)
+        self.cal_timezone_info = zoneinfo.ZoneInfo("UTC")
+
+        self.events_data: dict[str, ChronosEvent] = {}
+
+        self.client = None
+        self.calendar = None
+        self.principal = None
+
+        # derived from calendars.json
+        self.cal_primary = None
+        self.cal_name = None
+        self.cal_user = None
+        self.cal_passwd = None
+
+        self.force_time = False  # affects only 24h allday events
+        self.force_start = None
+        self.force_end = None
+
+        self.ignore_planned = False
+        self.ignore_descriptions = False
+        self.title_prefix = None
+        self.tags = []
+        self.color = None
+        self.default_location = None
+
+        self.tags_excluded = []
+
+        self.sanitize = {"stati": True, "source_icons": True, "target_icons": True}
+
+        self.icons = {}
---
+++ src/chronos/ics_calendar_handler.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# python lib
+from hashlib import md5
+from pathlib import Path
+from urllib.request import urlretrieve
+import datetime as dt
+import logging
+import time
+import zoneinfo
+
+# external libs
+import caldav
+import icalendar
+import x_wr_timezone
+
+# own code
+from chronos.base_calendar_handler import BaseCalendarHandler
+from chronos.config import Config
+from chronos.chronos_event import ChronosEvent
+
+
+class IcsCalendarHandler(BaseCalendarHandler):
+    def __init__(self, app_config: Config):
+        super().__init__(app_config)
